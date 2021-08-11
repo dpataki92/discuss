59f5ae934d671ec67db923725fce5670b5aaae2a
dpataki92 <ptamasdaniel92@gmail.com>
2021-08-11 23:20:43 +0200

    insert comments into the db

--- web/channels/comments_channel.ex
+++ web/channels/comments_channel.ex
@@ -1,15 +1,28 @@
 defmodule Discuss.CommentsChannel do
   use Discuss.Web, :channel
-  alias Discuss.Topic
+  alias Discuss.{Topic, Comment}
 
   def join("comments:" <> topic_id, _params, socket) do
     topic_id = String.to_integer(topic_id)
     topic = Repo.get(Topic, topic_id)
 
-    {:ok, %{}, socket}
+    {:ok, %{}, assign(socket, :topic, topic)}
   end
 
-  def handle_in(name, message, socket) do
+  def handle_in(_name, %{"content" => content}, socket) do
+    topic = socket.assigns.topic
+
+    changeset = topic
+    |> build_assoc(:comments)
+    |> Comment.changeset(%{content:  content})
+
+    case Repo.insert(changeset) do
+      {:ok, _comment} ->
+        {:reply, :ok, socket}
+      {:error, _reason} ->
+        {:reply, {:error, %{errors: changeset}}, socket}
+    end
+
     {:reply, :ok, socket}
   end
 end
